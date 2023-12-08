8ec6a717f28fd18b536a4fb7f7642ece0f9e9663
alanegd <alangalavisd@gmail.com>
2023-12-08 15:30:46 -0500

    Test commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,31 +3,8 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `SmartStock`,
-    description: `landing page made to learn gatsby`,
+    title: `gatsby-landing-page`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `SmartStock`,
-        start_url: `/`,
-        display: `minimal-ui`,
-        icon: `src/images/logo.svg`,
-      },
-    }
-    /*
-    "gatsby-plugin-postcss",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images/`,
-      },
-    },
-    */
-  ]
-}; 
+  plugins: ["gatsby-plugin-postcss"]
+};
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,10 @@
 import React from "react";
-import Layout from "../components/layout/Layout"
 
 export default function About(){
-  return (
-    <Layout>
-      <section>
+    return (
         <div>
-          <h1>About page</h1>
-          <p>Lorem ipsum</p>
+            <h1>About page</h1>
+            <p>Lorem ipsum</p>
         </div>
-      </section>
-    </Layout>
-  )
+    )
 }
